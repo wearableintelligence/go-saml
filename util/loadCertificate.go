b87cf97c975a68f2a77ba70160778c68735ed39e
Shealen Clare <shealen.clare@gmail.com>
2019-05-06 15:39:02 -0700

    Provide option to use cert chains
    I tested this with our actual private key and cert chain (before
    deleting them again) and it works. Just gotta add some example/dummy
    chain/key pair and uncomment the tests.

--- util/loadCertificate.go
+++ util/loadCertificate.go
@@ -28,15 +28,38 @@ func LoadCertificate(certPath string) (string, error) {
 func LoadCertificateChain(certPath string) ([]string, error) {
 	b, err := ioutil.ReadFile(certPath)
 	if err != nil {
-		return []string{""}, err
+		return []string{}, err
 	}
-	cert := string(b)
+	certFile := string(b)
 
-	re := regexp.MustCompile("---(.*)CERTIFICATE(.*)---")
-	cert = re.ReplaceAllString(cert, "")
-	cert = strings.Trim(cert, " \n")
-	cert = strings.Replace(cert, "\n", "", -1)
+	return SplitCertificateChain(certFile), nil
+}
 
-	// TODO make this read many certs instead of just one
-	return []string{cert}, nil
+// SplitCertificateChain from single certificate string
+// expect each cert to begin with `-----BEGIN CERTIFICATE-----\n`
+// and end with `-----END CERTIFICATE-----\n`
+func SplitCertificateChain(certFile string) []string {
+	certs := []string{}
+	var currentCert []string
+
+	startCertLine := regexp.MustCompile("---(.*)BEGIN CERTIFICATE(.*)---")
+	endCertLine := regexp.MustCompile("---(.*)END CERTIFICATE(.*)---")
+
+	certLines := strings.Split(certFile, "\n")
+	for _, line := range certLines {
+		if startCertLine.Match([]byte(line)) {
+			// start a new certificate
+			currentCert = []string{}
+
+		} else if endCertLine.Match([]byte(line)) {
+			// end a certificate by adding it to the list
+			certs = append(certs, strings.Join(currentCert, "\n"))
+
+		} else {
+			// append a certificate line to a cert
+			currentCert = append(currentCert, line)
+		}
+	}
+
+	return certs
 }
--- xmlsec_test.go
+++ xmlsec_test.go
@@ -29,6 +29,49 @@ func TestSingleCertRequest(t *testing.T) {
 	assert.NoError(err)
 }
 
+func TestChainCertRequestOfSingleCert(t *testing.T) {
+	assert := assert.New(t)
+	certs, err := util.LoadCertificateChain("./default.crt")
+	assert.NoError(err)
+
+	// Construct an AuthnRequest
+	authRequest := NewAuthnRequest()
+	authRequest.Signature[0].KeyInfo.X509Data.X509Certificates[0].Cert = certs[0]
+
+	b, err := xml.MarshalIndent(authRequest, "", "    ")
+	assert.NoError(err)
+	xmlAuthnRequest := string(b)
+
+	signedXml, err := SignRequest(xmlAuthnRequest, "./default.key")
+	assert.NoError(err)
+	assert.NotEmpty(signedXml)
+
+	err = VerifyRequestSignature(signedXml, "./default.crt")
+	assert.NoError(err)
+}
+
+// // This is how we'll test the cert-chain signing when we get some test certs
+//func TestChainCertRequestOfChainFile(t *testing.T) {
+//	assert := assert.New(t)
+//	certs, err := util.LoadCertificateChain("./chain.crt")
+//	assert.NoError(err)
+//
+//	// Construct an AuthnRequest
+//	authRequest := NewAuthnRequest()
+//	authRequest.Signature[0].KeyInfo.X509Data.X509Certificates = NewX509Certificates(certs)
+//
+//	b, err := xml.MarshalIndent(authRequest, "", "    ")
+//	assert.NoError(err)
+//	xmlAuthnRequest := string(b)
+//
+//	signedXml, err := SignRequest(xmlAuthnRequest, "./chain.key")
+//	assert.NoError(err)
+//	assert.NotEmpty(signedXml)
+//
+//	err = VerifyRequestSignature(signedXml, "./chain.crt")
+//	assert.NoError(err)
+//}
+
 func TestResponse(t *testing.T) {
 	assert := assert.New(t)
 	cert, err := util.LoadCertificate("./default.crt")
